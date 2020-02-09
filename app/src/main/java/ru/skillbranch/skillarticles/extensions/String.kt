1a5c9a9fa8fbe8e1af9eee72e575952988152a9e
korresh <kalegin.andrew@gmail.com>
2020-02-09 16:03:18 +0500

    commit_hometask2_lasttask

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,11 +1,13 @@
 package ru.skillbranch.skillarticles.extensions
 
+import java.util.*
+
 
 fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int>{
     this ?: return listOf()
     if (substr.isBlank()) return listOf()
     val searchResults = mutableListOf<Int>()
-    val searchString = if (ignoreCase)this.toLowerCase() else this
+    val searchString = if (ignoreCase)this.toLowerCase(Locale.getDefault()) else this
     for (index in 0 until(searchString.length - substr.length)){
       if (searchString.substring(index,index+substr.length) == substr ){
            searchResults.add(index)
