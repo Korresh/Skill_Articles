69115075601569a517c8e63b7159acf43f930631
korresh <kalegin.andrew@gmail.com>
2020-03-20 09:21:12 +0500

    hometask6

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/List.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/List.kt
@@ -2,26 +2,10 @@ package ru.skillbranch.skillarticles.extensions
 
 import kotlin.math.*
 
-fun List<Pair<Int, Int>>.groupByBounds(bounds: List<Pair<Int, Int>>): List<List<Pair<Int, Int>>> = bounds.map{boundary ->
-     this.filter { it.second >boundary.first && it.first < boundary.second }
-          .map { when{it.first < boundary.first -> Pair(boundary.first, it.second)
-               it.second > boundary.second -> Pair(it.first, boundary.second)
-               else -> it
-          }
-      }
-}
-
-/*fun List<Pair<Int, Int>>.groupByBounds(bouds: List<Pair<Int, Int>>): List<List<Pair<Int, Int>>> {
+fun List<Pair<Int, Int>>.groupByBounds(bouds: List<Pair<Int, Int>>): List<List<Pair<Int, Int>>> {
      val result =  bouds.map { (start, end) -> this.filter {  it.first >=start && it.second<=end } }
      return result
-}*/
+}
 
 
-/*fun List<Pair<Int,Int>>.groupByBounds(bounds: List<Pair<Int,Int>>): List<List<Pair<Int, Int>>> {
-     val result  = mutableListOf<List<Pair<Int,Int>>>()
-     bounds.forEach {(intervalLow, intervalHigh) ->
-          result.add(this
-               .filter{(foundLow, foundHigh) -> foundLow < intervalHigh && foundHigh > intervalLow}
-               .map{(low, high) -> Pair(max(intervalLow, low), min(intervalHigh, high))})}
-     return result
-}*/
+
