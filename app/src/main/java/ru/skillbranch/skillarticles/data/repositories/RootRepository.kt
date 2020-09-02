4bd8b8c0cd06e288c00a8cd80f5264e63612f4df
Korresh <kalegin.andrew@gmail.com>
2020-09-02 20:26:41 +0500

    hometask9 with* add shimmer

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/RootRepository.kt
@@ -5,6 +5,6 @@ import ru.skillbranch.skillarticles.data.local.PrefManager
 
 object RootRepository {
     val preferences = PrefManager
-    fun isAuth() : LiveData<Boolean> = preferences.isAuth()
+    fun isAuth() : LiveData<Boolean> = preferences.isAuthLive
     fun setAuth(auth:Boolean) { preferences.isAuth = auth }
 }
