0472546fb15aa35c20e61e36cbba6244feb0b5b3
Korresh <kalegin.andrew@gmail.com>
2020-09-02 17:33:22 +0500

    hometask9 with* add shimmer

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.distinctUntilChanged
 import androidx.preference.PreferenceManager
 import ru.skillbranch.skillarticles.App
@@ -22,10 +23,10 @@ object PrefManager {
     var isBigText by PrefDelegate(false)
 
 
-    val isAuthLive: LiveData<Boolean> by PrefLiveDelegate("isAuth",false, preferences)
+    //val isAuthLive: LiveData<Boolean> by PrefLiveDelegate("isAuth",false, preferences)
 
     fun getAppSettings() = MediatorLiveData<AppSettings>().apply{
-        val isDarkModeLive: LiveData<Boolean> by PrefLiveDelegate("isDarkMode",false, preferences)
+        /*val isDarkModeLive: LiveData<Boolean> by PrefLiveDelegate("isDarkMode",false, preferences)
         val isBigTextLive: LiveData<Boolean> by PrefLiveDelegate("isBigText",false, preferences)
         value = AppSettings()
 
@@ -34,13 +35,18 @@ object PrefManager {
         }
         addSource(isBigTextLive){
             value = value!!.copy(isBigText = it)
-        }
+        }*/
 
     }.distinctUntilChanged()
 
 
     fun clearAll() {
         preferences.edit().clear().apply()
+    }
+
+    fun isAuth(): MutableLiveData<Boolean> {
+        //TODO implement me
+        return MutableLiveData(false)
     }
 
 }
